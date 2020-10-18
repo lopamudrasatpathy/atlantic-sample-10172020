fabaa75aa10d048be45368cee628f43749c4d81f
lopamudrasatpathy <lopamudra.satpathy@gmail.com>
2020-10-17 21:41:37 -0500

    Initial version

---
+++ src/main/java/atlantic/sample10172020/Sample10172020Application.java
@@ -0,0 +1,13 @@
+package atlantic.sample10172020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample10172020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample10172020Application.class, args);
+	}
+
+}
---
+++ src/test/java/Atlantic/sample10172020/Sample10172020ApplicationTests.java
@@ -0,0 +1,13 @@
+package atlantic.sample10172020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample10172020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
